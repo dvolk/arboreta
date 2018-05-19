dc5e4689c6888e640b284b946dad1345526b2bf8
Denis Volk <denis.volk@gmail.com>
2018-05-19 22:22:44 +0100

    add db lock, add comments, merge func

--- main.py
+++ main.py
@@ -13,8 +13,12 @@ import time
 from flask import Flask, request
 
 con = sqlite3.connect('getree.sqlite', check_same_thread=False)
+db_lock = threading.Lock()
 
 def demon_interface():
+    #
+    # fetch sample neighbours from elephantwalk
+    #
     def get_neighbours(guid, reference, distance, quality, elephantwalkurl):
         url = "{0}/sample/findneighbour/snp/{1}/{2}/{3}/elephantwalk/{4}".format(elephantwalkurl, guid, reference, distance, quality)
         print("Url: {0}".format(url))
@@ -29,6 +33,9 @@ def demon_interface():
         print("Found {0} guids".format(len(guids)))
         return guids
 
+    #
+    # merge fasta files from guids into a multifasta file
+    #
     def concat_fasta(guids, reference, out_file):
         with open(out_file, "w") as out:
             for guid in guids:
@@ -48,15 +55,24 @@ def demon_interface():
                 out.write(">{0}\n".format(guid))
                 out.write("{0}\n".format(fasta))
 
+    #
+    # read tree file
+    #
     def _get_tree(guid, reference, distance, quality):
+        db_lock.acquire()
         elem = con.execute('select * from queue where sample_guid = ? and reference = ? and distance = ? and quality = ?',
                            (guid,reference,distance,quality)).fetchall()
+        con.commit()
+        db_lock.release()
         if not elem:
             print("invariant failed: _get_neighbours")
             exit(1)
         tree = open("./{0}/merged_fasta.treefile".format(elem[0][1])).read()
         return tree
 
+    #
+    # get neighbours, make multifasta file and run iqtree
+    #
     def go(guid, run_uuid, reference, distance, quality, elephantwalkurl, cores, iqtreepath):
         neighbour_guids = get_neighbours(guid, reference, distance, quality, elephantwalkurl)
 
@@ -78,9 +94,14 @@ def demon_interface():
         os.chdir(old_dir)
         return (ret, neighbour_guids)
 
+    #
+    # read queue table and run go() when there's an entry
+    #
     while True:
+        db_lock.acquire()
         elem = con.execute('select * from queue order by epoch_added desc limit 1').fetchall()
         con.commit()
+        db_lock.release()
 
         if elem:
             elem = elem[0]
@@ -90,11 +111,13 @@ def demon_interface():
                                       "../contrib/iqtree-1.6.5-Linux/bin/iqtree")
             ended = str(int(time.time()))
             tree = _get_tree(elem[0], elem[4], elem[5], elem[6])
+            db_lock.acquire()
             con.execute('delete from queue where sample_guid = ? and reference = ? and distance = ? and quality = ?',
                         (elem[0], elem[4], elem[5], elem[6]))
             con.execute('insert into complete values (?,?,?,?,?,?,?,?,?,?,?)',
                         (elem[0], elem[1], elem[3], elem[4], elem[5], elem[6], elem[7], started, ended, json.dumps(neighbour_guids), tree))
             con.commit()
+            db_lock.release()
             print("done with {0}", elem)
 
         print("sleeping")
@@ -105,8 +128,10 @@ T.start()
 
 app = Flask(__name__)
 
-@app.route('/neighbours/<guid>')
-def get_neighbours(guid):
+#
+# return nth column from run table. add run to queue if it doesn't exist
+#
+def get_run_index(guid, n):
     reference = request.args.get('reference')
     if not reference: reference = "R00000039"
     distance = request.args.get('distance')
@@ -114,48 +139,20 @@ def get_neighbours(guid):
     quality = request.args.get('quality')
     if not quality: quality = "0.08"
 
+    db_lock.acquire()
     queued = con.execute('select * from queue where sample_guid = ? and reference = ? and distance = ? and quality = ?',
                          (guid,reference,distance,quality)).fetchall()
     completed = con.execute('select * from complete where sample_guid = ? and reference = ? and distance = ? and quality = ?',
                             (guid,reference,distance,quality)).fetchall()
     con.commit()
+    db_lock.release()
 
     if queued and completed:
         print("invariant failed: queued and completed")
         exit(1)
 
     if completed:
-        return completed[0][9]
-    elif queued:
-        return "run is already queued\n"
-    else:
-        run_uuid = str(uuid.uuid4())
-        con.execute('insert into queue values (?,?,?,?,?,?,?)',
-                    (guid,run_guid,"queued",reference,distance,quality,str(int(time.time()))))
-        con.commit()
-        return "run added to queue\n"
-
-@app.route('/tree/<guid>')
-def get_tree(guid):
-    reference = request.args.get('reference')
-    if not reference: reference = "R00000039"
-    distance = request.args.get('distance')
-    if not distance: distance = "20"
-    quality = request.args.get('quality')
-    if not quality: quality = "0.08"
-
-    queued = con.execute('select * from queue where sample_guid = ? and reference = ? and distance = ? and quality = ?',
-                         (guid,reference,distance,quality)).fetchall()
-    completed = con.execute('select * from complete where sample_guid = ? and reference = ? and distance = ? and quality = ?',
-                            (guid,reference,distance,quality)).fetchall()
-    con.commit()
-
-    if queued and completed:
-        print("invariant failed: queued and completed")
-        exit(1)
-
-    if completed:
-        return completed[0][10]
+        return completed[0][n]
     elif queued:
         return "run is already queued\n"
     else:
@@ -165,12 +162,29 @@ def get_tree(guid):
         con.commit()
         return "run added to queue\n"
 
+#
+# flask routes
+#
+@app.route('/neighbours/<guid>')
+def get_neighbours(guid):
+    return get_run_index(guid, 9)
+
+@app.route('/tree/<guid>')
+def get_tree(guid):
+    return get_run_index(guid, 10)
+
 @app.route('/queue')
 def get_queue():
+    db_lock.acquire()
     queued = con.execute('select sample_guid, reference, distance, quality from queue').fetchall()
+    con.commit()
+    db_lock.release()
     return json.dumps(queued)
 
 @app.route('/complete')
 def get_complete():
+    db_lock.acquire()
     complete = con.execute('select sample_guid, reference, distance, quality from complete').fetchall()
+    con.commit()
+    db_lock.release()
     return json.dumps(complete)
